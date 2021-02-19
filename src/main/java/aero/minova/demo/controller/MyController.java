1ee3b6b564d508c37cb0c761b3d79ebc4d79afa8
Wilfried Saak <saak@minova.aero>
2021-02-19 20:57:11 +0100

    RestController einbauen und dokumentieren

---
+++ src/main/java/aero/minova/demo/controller/MyController.java
@@ -0,0 +1,14 @@
+package aero.minova.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping(value = "/")
+    public String getRoot() {
+        return "Hello World";
+    }
+
+}
